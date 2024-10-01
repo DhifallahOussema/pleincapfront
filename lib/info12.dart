f26426f4d4ca8fd8ac6f7698ac2c7dacfd6d8c34
Dhifallah Oussema <103474993+DhifallahOussema@users.noreply.github.com>
2024-10-01 12:22:26 +0100

    one other tester

--- lib/info12.dart
+++ lib/info12.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'formations.dart';
-
+// s
 class Info12Page extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
